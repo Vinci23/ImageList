e78c49ccf460a522e3a37f7c1c9192518a5fdb0e
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-01-16 18:47:53 +0100

    Add files via upload

---
+++ ImageList/ImageList/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ImageList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.buttonPromijeni1 = new System.Windows.Forms.Button();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(141, 73);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(220, 147);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // buttonPromijeni1
+            // 
+            this.buttonPromijeni1.Location = new System.Drawing.Point(141, 249);
+            this.buttonPromijeni1.Name = "buttonPromijeni1";
+            this.buttonPromijeni1.Size = new System.Drawing.Size(220, 23);
+            this.buttonPromijeni1.TabIndex = 1;
+            this.buttonPromijeni1.Text = "Promijeni sliku";
+            this.buttonPromijeni1.UseVisualStyleBackColor = true;
+            this.buttonPromijeni1.Click += new System.EventHandler(this.buttonPromijeni1_Click);
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Black;
+            this.imageList.Images.SetKeyName(0, "2020-Yamaha-R6.jpg");
+            this.imageList.Images.SetKeyName(1, "yamaha_mt-07.jpg");
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(505, 316);
+            this.Controls.Add(this.buttonPromijeni1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonPromijeni;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button buttonPromijeni1;
+        public System.Windows.Forms.ImageList imageList;
+    }
+}
+
